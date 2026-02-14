8d96b31ab7939bd3e167e15eea4c3351070eddf4
Karthik Surya <your-github-email@gmail.com>
2026-02-14 12:09:18 +0530

    Updated API URL for production

--- src/app/patientcare-services/book-appointment/request-appointment/appointment-form/appointment-form.component.ts
+++ src/app/patientcare-services/book-appointment/request-appointment/appointment-form/appointment-form.component.ts
@@ -264,30 +264,3 @@ export class AppointmentFormComponent {
     return `${year}-${month}-${day}`;
   }
 }
-
-// import com.twilio.Twilio;
-// import com.twilio.converter.Promoter;
-// import com.twilio.rest.api.v2010.account.Message;
-// import com.twilio.type.PhoneNumber;
-
-// import java.net.URI;
-// import java.math.BigDecimal;
-
-// public class Example {
-//   // Find your Account Sid and Token at twilio.com/console
-//   public static final String ACCOUNT_SID = "AC2875bffb75274dc1f3ffe2ab60698131";
-//   public static final String AUTH_TOKEN = "[AuthToken]";
-
-//   public static void main(String[] args) {
-//     Twilio.init(ACCOUNT_SID, AUTH_TOKEN);
-//     Message message = Message.creator(
-//       new com.twilio.type.PhoneNumber("whatsapp:+919080564314"),
-//       new com.twilio.type.PhoneNumber("whatsapp:+14155238886"),
-//       "HXb5b62575e6e4ff6129ad7c8efe1f983e",
-//       "{"1":"12/1","2":"3pm"}",
-//       "Your message")
-//     .create();
-
-//     System.out.println(message.getSid());
-//   }
-// }
